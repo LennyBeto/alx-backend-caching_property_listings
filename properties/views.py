5b0bfe7f07392bb4bae8c88e69d4bf01447205a0
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-08-31 23:34:15 +0300

    Update views.py

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from .utils import get_all_properties
 
-# Create your views here.
+# Cache the entire view for 15 minutes
+@cache_page(60 * 15)
+def property_list(request):
+    properties = get_all_properties()
+    return JsonResponse({"data": properties}, safe=False)
